5ed34524895e131deff7ae0a37aaa7f429bac4ae
NKVDream <Kmv235@yandex.ru>
2025-04-14 07:35:05 -0700

    first try

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Task1
+{
+    static void Main()
+    {
+        Console.Write("write down anything:");string X = Console.ReadLine();
+        Task1.check(X);
+    }
+    static bool check(string X)
+    {
+        string Firstword = "English";
+        string Fiend = "";
+        for (int i = 0; i < X.Length; i++)
+        {
+            Console.Write(X[i]);
+            if (X[i] == 'E')
+            {
+                Fiend += X[i];
+                continue;
+            }
+            if (X[i] == 'n')
+            {
+                Fiend += X[i];
+                continue;
+            }
+            if (Fiend = "En")
+            {
+                return true;
+            }
+        }
+    }
+}
