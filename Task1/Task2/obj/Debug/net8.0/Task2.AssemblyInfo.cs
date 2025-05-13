90a5c540b709b812b98d02234d6c3b0bd188f7ca
NKVDream <Kmv235@yandex.ru>
2025-05-13 06:48:49 -0700

    3 done

--- Task1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18081897eada826d74030a468c84a1c2a897bdc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ff943aa91e34332e0a540f5a11481818b4ad83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task1/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Task3
+{
+    static void Main()
+    {
+        Console.Write("Write down any word: ");
+        string Y = Console.ReadLine();
+        string result = Task3.Del(Y, out string resY);
+        Console.Write(result);
+    }
+    static string Del(string y, out string resY)
+    {
+        int StrL = y.Length;
+        string NewYu = "";
+        string NewYl = "";
+        for (int i = 0; i < StrL; i++)
+        {
+            if (i %2 == 0)
+            {
+                NewYu += y.ToUpper()[i];
+                continue;
+            }
+            else if ((StrL - 1) %2 != 0)
+            {
+                NewYu += y.ToLower()[i];
+            }
+        }
+        for (int i = 0; i < StrL; i++)
+        {
+            if (i % 2 == 0)
+            {
+                NewYl += y.ToLower()[i];
+                continue;
+            }
+            else if ((StrL - 1) % 2 != 0)
+            {
+                NewYl += y.ToUpper()[i];
+            }
+        }
+        resY = ($"'{y}'->'{NewYu}','{NewYl}'");
+        return resY;
+
+
+    }
+}
